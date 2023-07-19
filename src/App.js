69433f04288e595cbf81c4da669f603d72fe7f4f
Denis Latyshau <denislatyshau@yandex.ru>
2023-07-19 15:01:34 +0300

    feat: add PostList + PostItem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import PostList from "./components/PostList";
+
+import './styles/App.css'
 
 function App() {
-  return 
+  const [posts, setPosts] = useState([
+          {id: 1, title: 'Title 1', body: 'Description'},
+          {id: 2, title: 'Title 2', body: 'Description'},
+          {id: 3, title: 'Title 3', body: 'Description'}
+    ])
+
+  return (
+    <div className="App">
+      <PostList posts={posts} title='Список постов'/>
+    </div>
+  )
 }
 
 export default App;
