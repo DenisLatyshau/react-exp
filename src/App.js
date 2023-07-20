1cf8f179cc8611db2313dff1c9959c8449a9c855
Denis Latyshau <denislatyshau@yandex.ru>
2023-07-20 17:11:55 +0300

    feat: add MySelect.jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect";
 
 import './styles/App.css'
 
@@ -10,6 +11,8 @@ function App() {
           {id: 2, title: 'Title 2', body: 'Description'},
           {id: 3, title: 'Title 3', body: 'Description'}
     ])
+
+  const [selectedSort, setSelectedSort] = useState('')
   
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -19,9 +22,26 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id) )
   }
 
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MySelect 
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue='Сортировка по'
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'}
+          ]}
+        />
+      </div>
       { posts.length
         ? <PostList remove={removePost} posts={posts} title='Посты про JS'/>
         : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1> }
