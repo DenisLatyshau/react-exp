a8776bb7c285a9e0c9597dcefc5d7f60b5c2af5a
Denis Latyshau <denislatyshau@yandex.ru>
2023-07-20 16:01:23 +0300

    feat: add PostForm + removePost

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import PostList from "./components/PostList";
-import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/inout/MyInput";
+import PostForm from "./components/PostForm";
 
 import './styles/App.css'
-
 
 function App() {
   const [posts, setPosts] = useState([
@@ -13,32 +11,21 @@ function App() {
           {id: 3, title: 'Title 3', body: 'Description'}
     ])
   
-  const [post, setPost] = useState( {title: '', body: ''} )
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost])
+  }
 
-  const addNewPost = (e) => {
-    e.preventDefault()
-    setPosts([...posts, {...post, id: Date.now()}])
-    setPost({title: '', body: ''})
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id) )
   }
 
   return (
     <div className="App">
-      <form>
-        <MyInput 
-            type="text" 
-            placeholder="Нзвание поста" 
-            value={post.title}
-            onChange={e => setPost({...post, title: e.target.value})}
-        />
-        <MyInput 
-            type="text" 
-            placeholder="Описание поста" 
-            value={post.body}
-            onChange={e => setPost({...post, body: e.target.value})}
-        />
-        <MyButton onClick={addNewPost} >Создать пост!</MyButton>
-      </form>
-      <PostList posts={posts} title='Посты про JS'/>
+      <PostForm create={createPost} />
+      { posts.length
+        ? <PostList remove={removePost} posts={posts} title='Посты про JS'/>
+        : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1> }
+      
     </div>
   )
 }
