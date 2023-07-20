3508125eec980855c54516bc4d38f4e91f8c30d1
Denis Latyshau <denislatyshau@yandex.ru>
2023-07-20 14:23:13 +0300

    feat: add input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/inout/MyInput";
 
 import './styles/App.css'
+
 
 function App() {
   const [posts, setPosts] = useState([
@@ -9,10 +12,33 @@ function App() {
           {id: 2, title: 'Title 2', body: 'Description'},
           {id: 3, title: 'Title 3', body: 'Description'}
     ])
+  
+  const [post, setPost] = useState( {title: '', body: ''} )
+
+  const addNewPost = (e) => {
+    e.preventDefault()
+    setPosts([...posts, {...post, id: Date.now()}])
+    setPost({title: '', body: ''})
+  }
 
   return (
     <div className="App">
-      <PostList posts={posts} title='Список постов'/>
+      <form>
+        <MyInput 
+            type="text" 
+            placeholder="Нзвание поста" 
+            value={post.title}
+            onChange={e => setPost({...post, title: e.target.value})}
+        />
+        <MyInput 
+            type="text" 
+            placeholder="Описание поста" 
+            value={post.body}
+            onChange={e => setPost({...post, body: e.target.value})}
+        />
+        <MyButton onClick={addNewPost} >Создать пост!</MyButton>
+      </form>
+      <PostList posts={posts} title='Посты про JS'/>
     </div>
   )
 }
