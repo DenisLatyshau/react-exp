eda374abbe707ecdfd55ab2ecb612de3d127b6c6
Denis Latyshau <denislatyshau@yandex.ru>
2023-07-24 11:53:00 +0300

    feat: add usePosts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
 
 import './styles/App.css'
+
 
 
 function App() {
@@ -17,18 +19,9 @@ function App() {
 
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
-  const sortedPosts = useMemo(() => {
-    if (filter.sort) {
-      return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-    }
-    return posts
-  }, [filter.sort, posts]) 
 
-  const sortedAndSearchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLocaleLowerCase().includes(filter.query.toLocaleLowerCase()))
-  }, [filter.query, sortedPosts])
-  
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
     setModal(false)
@@ -56,7 +49,6 @@ function App() {
         posts={sortedAndSearchedPosts} 
         title='Посты про JS'
       />
-      
     </div>
   )
 }
